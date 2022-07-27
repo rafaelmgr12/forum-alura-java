fe49ad08a3d8f68eecc6c07aeb5667eb8ba583b0
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2022-07-27 17:45:42 -0300

    first commit

---
+++ src/main/java/com/rafaelmgr12/forumalurajava/ForumAluraJavaApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelmgr12.forumalurajava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumAluraJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForumAluraJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rafaelmgr12/forumalurajava/ForumAluraJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelmgr12.forumalurajava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumAluraJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
